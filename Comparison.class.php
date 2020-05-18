c2f06348abf3cd1ddbc30a6e4d3cf57d5811dcfa
NielzosFilms <nielzosfilms@gmail.com>
2020-05-18 12:06:07 +0200

    automatic threshold, test files added

--- Comparison.class.php
+++ Comparison.class.php
@@ -1,17 +1,13 @@
 <?php
 /**
- * Compares 2 variables of code
+ * Compares 2 variables of code and outputs a % and boolean
  *
- * Still needs to be able to look through a .php file and make special cases for variable, function names
+ * Still needs to be able to look through a .php file
+ * and make special cases for variable, function names
  */
 class Comparison
 {
     private $thresh;
-
-    public function __construct($thresh)
-    {
-        $this->thresh = $thresh;
-    }
 
     public function compare($code_1, $code_2)
     {
@@ -30,6 +26,27 @@ class Comparison
 
     public function detectThreshold($code_1, $code_2)
     {
+        $len_diff = strlen($code_1) - strlen($code_2);
+
+        echo "length diff = ".$len_diff.PHP_EOL;
+
+        $new_thresh = $this->map($len_diff, 0, 1000, 50, 100);
+        echo "new threshold = " . $new_thresh.PHP_EOL;
+
+        $this->thresh = $new_thresh;
+    }
+
+    private function map($value, $fromLow, $fromHigh, $toLow, $toHigh)
+    {
+        $fromRange = $fromHigh - $fromLow;
+        $toRange = $toHigh - $toLow;
+        $scaleFactor = $toRange / $fromRange;
+
+        $tmpValue = $value - $fromLow;
+        
+        $tmpValue *= $scaleFactor;
+
+        return $tmpValue + $toLow;
     }
 
     public function setThreshold($thresh)
--- test.php
+++ test.php
@@ -6,9 +6,26 @@ spl_autoload_register(
     }
 );
 
-$text_1 = "Dit is een test!";
-$text_2 = "Dit Hello World!";
+$text_1 = file_get_contents(
+    "Test_files\script_1.js"
+);
 
-$comparer = new Comparison(10);
+$text_2 = file_get_contents(
+    "Test_files\script_2.js"
+);
+
+echo "File 1 length: ".strlen($text_1).PHP_EOL;
+echo "File 2 length: ".strlen($text_2).PHP_EOL;
+echo PHP_EOL;
+
+$comparer = new Comparison();
+
+//Automatically detect threshold
 $comparer->detectThreshold($text_1, $text_2);
-echo $comparer->compare($text_1, $text_2)[0];
+
+//Calculate dupe percentage
+$result = $comparer->compare($text_1, $text_2);
+
+echo PHP_EOL;
+echo "Dupe: ".($result[0] ? "Yes" : "No").PHP_EOL;
+echo "Percentage: ".$result[1].PHP_EOL;
