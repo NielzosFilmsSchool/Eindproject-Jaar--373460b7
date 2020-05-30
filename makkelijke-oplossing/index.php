d06e78b7597fcbca0b981e9c8a444c0651af5ea8
LyxurD4 <mdoes@roc-dev.com>
2020-05-30 13:20:18 +0200

    progress

---
+++ makkelijke-oplossing/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>BRUH</h1>
+    <form action="test.php" method="POST">
+        <input type="text" name="link">
+        <input type="text" name="fileName">
+        <input type="submit" name="submit" value="Submit">
+    </form>
+</body>
+</html>
