957a7326e76c8c122d431d3e2698927d02b348a6
NielzosFilms <nielzosfilms@gmail.com>
2020-06-10 10:33:59 +0200

    feature final file

--- Comparison.class.php
+++ Comparison.class.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * Compares 2 variables of code and outputs a % and boolean
+ * Compares 2 array's of code and outputs a % and boolean
  *
- * Still needs to be able to look through a .php file
- * and make special cases for variable, function names
+ * Cast array to lower case
+ * Currently checks all lines that are simular
  */
 class Comparison
 {
@@ -14,24 +14,36 @@ class Comparison
         $result = 0;
         $simular = false;
 
-        similar_text($code_1, $code_2, $result);
+        $linesFound = 0;
+        foreach ($code_2 as $line_2) {
+            if (in_array($line_2, $code_1)) {
+                $linesFound++;
+            }
+        }
+        
+        echo "Simular lines found = $linesFound";
+        echo PHP_EOL;
+
+        $result = (100 / count($code_1) * $linesFound);
+        $result = round($result*10)/10;
 
         if ($result > $this->thresh) {
             $simular = true;
         }
 
-        $output = array($simular, $result);
-        return $output;
+        return array($simular, $result);
     }
 
     public function detectThreshold($code_1, $code_2)
     {
-        $len_diff = abs(strlen($code_1) - strlen($code_2));
+        $len_diff = abs(count($code_1) - count($code_2));
 
-        echo "length diff = ".$len_diff.PHP_EOL;
+        echo "Length diff = ".$len_diff.PHP_EOL;
 
-        $new_thresh = $this->map($len_diff, 0, 1000, 50, 100);
-        echo "new threshold = " . $new_thresh.PHP_EOL;
+        $new_thresh = $this->map($len_diff, 0, 500, 75, 100);
+        $new_thresh = round($new_thresh*10)/10;
+        
+        echo "New threshold = " . $new_thresh.PHP_EOL;
 
         $this->thresh = $new_thresh;
     }
