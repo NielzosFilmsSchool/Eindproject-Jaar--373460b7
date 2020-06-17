b86aeb255123e2c7134b95199cf6a48d4f2691b4
NielzosFilms <nielzosfilms@gmail.com>
2020-06-17 11:23:10 +0200

    showing all the entries in a table on the index.php

--- index.php
+++ index.php
@@ -14,6 +14,42 @@
         <input type="text" name="fileName" placeholder="Filename...">
         <input type="submit" name="submit" value="Submit">
     </form>
+
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>Username</th>
+            <th>Repo Link</th>
+            <th>Exercise Name</th>
+            <th>Filename</th>
+            <th>Dupe Percentage</th>
+            <th>Dupe ID</th>
+            <th>Dupe</th>
+        </tr>
+        <?php
+        $dsn = "mysql:host=localhost;dbname=dupe_comparison";
+        $user = "root";
+        $passwd = "";
+        $pdo = new PDO($dsn, $user, $passwd);
+
+        $exercises = $pdo->query("SELECT * FROM exercise");
+        while ($row = $exercises->fetch()) {
+            ?>
+        <tr>
+            <td><?= $row["id"]?></td>
+            <td><?= $row["username"]?></td>
+            <td><?= $row["link"]?></td>
+            <td><?= $row["exercise_name"]?></td>
+            <td><?= $row["filename"]?></td>
+            <td><?= $row["highest_dupe_percentage"]?></td>
+            <td><?= $row["dupe_exercise_id"]?></td>
+            <td><?= $row["dupe"]?></td>
+        </tr>
+        <?php
+        }
+        ?>
+    </table>
+
 </body>
 
 </html>
