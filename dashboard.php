5c17fda68c03791f38a5f609006b21453131e677
NielzosFilms <nielzosfilms@gmail.com>
2020-07-01 11:54:42 +0200

    dashboard

--- dashboard.php
+++ dashboard.php
@@ -1,46 +1,81 @@
-<!DOCTYPE html>
-<html lang="en">
-
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Comparison Test</title>
+    <meta charset="utf-8">
+    <title>Jarvis | Bit Academy</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/dashboard.css" rel="stylesheet">
 </head>
 
 <body>
-    <h1>Comparison Test</h1>
-    <form action="codeFromGithub.php" method="GET">
-        <input type="text" name="repo_link" placeholder="Link...">
-        <input type="submit" name="submit" value="Submit">
-    </form>
+    <header>
+        <img src="img/logo.png" alt="Bit Logo" class="logo">
+    </header>
+    <main>
+        <h1 style="padding: 20px;">Dashboard</h1>
+        <div class="table">
+            <table>
+                <tr>
+                    <td>Username</td>
+                    <td>Exercise Name</td>
+                    <td>Repo Link</td>
+                    <td>Plagiaat</td>
+                    <td>Files</td>
+                </tr>
+                <?php
+                $dsn = "mysql:host=localhost;dbname=dupe_comparison";
+                $user = "root";
+                $passwd = "";
+                $pdo = new PDO($dsn, $user, $passwd);
 
-    <table>
-        <tr>
-            <th>ID</th>
-            <th>Username</th>
-            <th>Repo Link</th>
-            <th>Exercise Name</th>
-        </tr>
-        <?php
-        $dsn = "mysql:host=localhost;dbname=dupe_comparison";
-        $user = "root";
-        $passwd = "";
-        $pdo = new PDO($dsn, $user, $passwd);
-
-        $exercises = $pdo->query("SELECT * FROM exercise");
-        while ($row = $exercises->fetch()) {
-            ?>
-        <tr>
-            <td><?= $row["id"]?></td>
-            <td><?= $row["username"]?></td>
-            <td><?= $row["link"]?></td>
-            <td><?= $row["exercise_name"]?></td>
-        </tr>
-        <?php
-        }
-        ?>
-    </table>
-
+                $exercises = $pdo->query("SELECT * FROM exercise");
+                while ($row = $exercises->fetch()) {
+                    $files = $pdo->query("SELECT * FROM files WHERE exercise_id = ".$row["id"]);
+                    $dupe = "Nee";
+                    while ($file = $files->fetch()) {
+                        if ($file["dupe"] == 1) {
+                            $dupe = "Ja";
+                        }
+                    } ?>
+                <tr>
+                    <td><?= $row["username"]?></td>
+                    <td><?= $row["exercise_name"]?></td>
+                    <td><a href="<?= $row["link"]?>"><?= $row["link"]?></a></td>
+                    <td><?= $dupe?></td>
+                    <td style="width: 40%;">
+                        <details>
+                            <summary>File Details</summary>
+                            <table class="files_table">
+                                <tr>
+                                    <td>Filename</td>
+                                    <td>Percentage</td>
+                                    <td>Plagiaat</td>
+                                    <td>Persoon</td>
+                                </tr>
+                                <?php
+                               $files = $pdo->query("SELECT * FROM files WHERE exercise_id = ".$row["id"]);
+                    while ($file = $files->fetch()) {
+                        ?>
+                                <tr>
+                                    <td><?= $file["filename"]?></td>
+                                    <td><?= $file["dupe_percentage"]?></td>
+                                    <td><?= $file["dupe"]?></td>
+                                    <td>
+                                        <?php
+                                        $person = $pdo->query("SELECT * FROM exercise WHERE id = ".$file["dupe_exercise_id"]);
+                        $person = $person->fetch();
+                        echo $person["username"]; ?>
+                                    </td>
+                                </tr>
+                                <?php
+                    } ?>
+                            </table>
+                        </details>
+                    </td>
+                </tr>
+                <?php
+                }
+                ?>
+            </table>
+        </div>
+    </main>
 </body>
-
-</html>
